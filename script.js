2b9eda30ad978c8c01d8791147f451104c66c780
= <=>
2025-10-17 21:11:31 +0700

    menambah asset

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const PASSWORD = '2359';
     const paragraphs = [
-      `Kitab Mandala Prakasa - sebuah peninggalan agung yang menyimpan
+      `Ada kalanya sesuatu yang paling berharga bukanlah apa yang terlihat,
+melainkan apa yang disembunyikan dengan penuh kehati-hatian.
+Kitab Mandala Prakasa - sebuah peninggalan agung yang menyimpan
 nilai-nilai luhur, telah lama disebut-sebut sebagai "hilang". Lokasinya menjadi
 teka-teki. Keberadaannya menjadi misteri. Namun apakah benar ia hilang?
 Ataukah ia hanya menunggu untuk ditemukan oleh mereka yang benar-benar layak?`,
+      `Kitab ini bukan sekadar naskah kuno. Ia adalah saksi bisu perjalanan para
+pemimpin terdahulu. Ia mencatat jejak kejujuran dalam kegelapan, keberanian
+dalam ketakutan, dan pengabdian dalam keheningan. Kitab ini adalah cermin.
+Bukan untuk melihat wajah, tetapi untuk melihat ke dalam jiwa.
+Dan sesungguhnya… kitab itu tidak pernah dicuri. Ia tidak pernah lenyap.`,
+      `Ia kami simpan dengan sengaja. Bukan untuk disembunyikan, tapi untuk
+dijaga dari tangan yang belum siap. Kami menyimpannya bukan karena takut
+kehilangannya, tapi karena kami ingin menghidupkannya kembali, bukan lewat
+mata, tapi lewat pengalaman, pemahaman, dan proses pembentukan diri.`,
+      `Tempat "hilangnya" kitab itu, kini bukan lagi ruang kosong. Ia adalah ruang
+ujian. Sebuah arena bagi para peserta PTA untuk mencari, bertanya, meragukan,
+dan akhirnya mengerti. Karena dalam proses pencarian itulah, terbentuk karakter.
+Dan dalam ketidakhadiran fisik itulah, justru nilai sejati kitab ini bisa hadir lebih
+utuh.
+Maka jangan cari Mandala Prakasa hanya dengan langkah kaki, tapi carilah
+ia dengan langkah hati. Karena sejatinya, kitab itu tidak berada di ujung pencarian,
+tapi ada di sepanjang perjalanan.`,
     ];
     const TYPE_SPEED = 50;
     const PAUSE_AFTER_PARAGRAPH = 2500;
-    const LOOP = true;
+    const LOOP = false;
 
     const overlay = document.getElementById('overlay');
     const passInput = document.getElementById('passwordInput');
@@ -41,7 +60,7 @@ Ataukah ia hanya menunggu untuk ditemukan oleh mereka yang benar-benar layak?`,
         startSequence();
       } else {
         passInput.value = '';
-        passInput.placeholder = 'kata sandi salah — coba lagi';
+        passInput.placeholder = 'Wrong Answer! -  Try Again!';
       }
     }
 
